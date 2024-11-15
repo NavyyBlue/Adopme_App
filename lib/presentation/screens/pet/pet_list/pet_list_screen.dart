063aa8d43bdc241b70e3995ca021801a44459ef2
NavyBlue <jean.cardenas2@unmsm.edu.pe>
2024-11-14 22:14:36 -0500

    Enlace de la vista Lista de mascotas con detalle de la mascota

--- lib/presentation/screens/pet/pet_list/pet_list_screen.dart
+++ lib/presentation/screens/pet/pet_list/pet_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:adopme_frontend/presentation/screens/pet/pet_list/pet_list_controller.dart';
+import 'package:adopme_frontend/presentation/screens/profile/pet_profile_screen.dart';
 
 class PetListScreen extends StatelessWidget {
   @override
@@ -33,84 +34,89 @@ class PetListScreen extends StatelessWidget {
                 itemCount: controller.pets.length,
                 itemBuilder: (context, index) {
                   var pet = controller.pets[index];
-                  return Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      elevation: 4,
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(12),
-                            child: Image.network(
-                              pet['imageUrl'],
-                              height: 100,
-                              width: 100,
-                              fit: BoxFit.cover,
+                  return GestureDetector(
+                    onTap: () {
+                      Get.to(() => PetProfileScreen());
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        elevation: 4,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(12),
+                              child: Image.network(
+                                pet['imageUrl'],
+                                height: 100,
+                                width: 100,
+                                fit: BoxFit.cover,
+                              ),
                             ),
-                          ),
-                          SizedBox(width: 16),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      pet['name'],
-                                      style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold,
+                            SizedBox(width: 16),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        pet['name'],
+                                        style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                        ),
                                       ),
-                                    ),
-                                    IconButton(
-                                      icon: Icon(
-                                        controller.favoritePets.contains(pet['id'])
-                                            ? Icons.favorite
-                                            : Icons.favorite_border,
-                                        color: Colors.red,
+                                      IconButton(
+                                        icon: Icon(
+                                          controller.favoritePets.contains(pet['id'])
+                                              ? Icons.favorite
+                                              : Icons.favorite_border,
+                                          color: Colors.red,
+                                        ),
+                                        onPressed: () {
+                                          controller.toggleFavorite(pet['id']);
+                                        },
                                       ),
-                                      onPressed: () {
-                                        controller.toggleFavorite(pet['id']);
-                                      },
-                                    ),
-                                  ],
-                                ),
-                                Text(
-                                  pet['breed'],
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.grey[600],
+                                    ],
                                   ),
-                                ),
-                                Text(
-                                  pet['age'],
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.grey[600],
-                                  ),
-                                ),
-                                Row(
-                                  children: [
-                                    Icon(Icons.location_on, size: 16, color: Colors.grey),
-                                    SizedBox(width: 4),
-                                    Text(
-                                      pet['distance'],
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        color: Colors.grey[600],
-                                      ),
+                                  Text(
+                                    pet['breed'],
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.grey[600],
                                     ),
-                                  ],
-                                ),
-                              ],
+                                  ),
+                                  Text(
+                                    pet['age'],
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.grey[600],
+                                    ),
+                                  ),
+                                  Row(
+                                    children: [
+                                      Icon(Icons.location_on, size: 16, color: Colors.grey),
+                                      SizedBox(width: 4),
+                                      Text(
+                                        pet['distance'],
+                                        style: TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.grey[600],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   );
