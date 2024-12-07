842bec79e53a644d6c7922e1134c34fa13087803
Erly75 <erlyrivera.750@gmail.com>
2024-12-06 19:09:02 -0500

    vista para las noticias de facebook y scrapping

--- lib/constants/network_constants.dart
+++ lib/constants/network_constants.dart
@@ -1,11 +1,13 @@
 class NetworkConstants{
-  static const String baseUrl = "http://192.168.1.8:3000";
+  static const String baseUrl = "http://192.168.1.44:3000";
   static const String analyzerImageRoute = "/analyzer-image";
   static const String getFeaturesPetEndpoint = "/get-features-pet";
   static const String comparePetsEndpoint = "/compare-pets";
+  static const String noticiasCompletasEndpoint = "/noticias_completas";
   static const String vetRoute = "/vets";
   static const String reviewsRoute = "/reviews";
   static const String preferencesRoute = "/preferences";
+  static const String noticiasRoute = "/noticias";
   static const String reviewsVetEndpoint = "/vet/{vetId}";
   static const String petRoute = "/pets";
   static const String petCreateEndpoint = "/create";
--- lib/presentation/screens/feed/feed_screen.dart
+++ lib/presentation/screens/feed/feed_screen.dart
@@ -1,7 +1,477 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:adopme_frontend/presentation/screens/feed/feed_controller.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+
+import 'dart:convert';  // Para convertir la respuesta JSON
+import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
+
+
+
+class FeedScreen extends StatefulWidget {
+  @override
+  _FeedScreenState createState() => _FeedScreenState();
+}
+
+class _FeedScreenState extends State<FeedScreen> {
+  var facebookData;
+  var completeData;
+
+  // Método para obtener datos de "noticias facebook"
+  Future<void> fetchFacebookData() async {
+    try {
+      final response = await http.get(Uri.parse('http://10.0.2.2:8000/noticias/noticias_facebook/'));
+      if (response.statusCode == 200) {
+        setState(() {
+          final decodedResponse = jsonDecode(utf8.decode(response.bodyBytes));
+          facebookData = decodedResponse;
+        });
+      } else {
+        print('Error Facebook: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error Facebook: $e');
+    }
+  }
+
+  // Método para obtener datos de "noticias completas"
+  Future<void> fetchCompleteData() async {
+    try {
+      final response = await http.get(Uri.parse('http://10.0.2.2:8000/noticias/noticias_completas/'));
+      if (response.statusCode == 200) {
+        setState(() {
+          final decodedResponse = jsonDecode(utf8.decode(response.bodyBytes));
+          completeData = decodedResponse;
+        });
+      } else {
+        print('Error Completo: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error Completo: $e');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchFacebookData();
+    fetchCompleteData();
+  }
+
+  Widget buildFacebookTab() {
+    return Center(
+      child: facebookData == null
+          ? CircularProgressIndicator()
+          : ListView.builder(
+              itemCount: facebookData['noticias'].length,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () async {
+                    final url = facebookData['noticias'][index]['imagenURL'];
+                    if (await canLaunch(url)) {
+                      await launch(url);
+                    } else {
+                      throw 'No se pudo abrir el enlace: $url';
+                    }
+                  },
+                  child: Card(
+                    margin: EdgeInsets.all(15),
+                    child: SizedBox(
+                      child: Row(
+                        children: [
+                          SizedBox(width: 10.0),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(facebookData['noticias'][index]['resu_noticia'],
+                                    style: TextStyle(fontWeight: FontWeight.bold)),
+                                Text(facebookData['noticias'][index]['date_noticia']),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+    );
+  }
+
+  Widget buildCompleteTab() {
+    return Center(
+      child: completeData == null
+          ? CircularProgressIndicator()
+          : ListView.builder(
+              itemCount: completeData['noticias'].length,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () async {
+                    final url = completeData['noticias'][index]['link'];
+                    if (await canLaunch(url)) {
+                      await launch(url);
+                    } else {
+                      throw 'No se pudo abrir el enlace: $url';
+                    }
+                  },
+                  child: Card(
+                    child: SizedBox(
+                      height: 100,
+                      child: Row(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(8.0),
+                            child: Image.network(
+                              completeData['noticias'][index]['imagen'],
+                              width: 100,
+                              height: 100,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          SizedBox(width: 8.0),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(completeData['noticias'][index]['titulo'],
+                                    style: TextStyle(fontWeight: FontWeight.bold)),
+                                Text(completeData['noticias'][index]['fecha'] +
+                                    ' ' +
+                                    completeData['noticias'][index]['hora']),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('AdoptMe'),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'Noticias Facebook'),
+              Tab(text: 'Noticias Diarios'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            buildFacebookTab(),
+            buildCompleteTab(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
+// crea una clase FeedScreen que extiende un StatelessWidget
+class FeedScreen extends StatefulWidget {
+  @override
+  _FeedScreenState createState() => _FeedScreenState();
+}
+
+class _FeedScreenState extends State<FeedScreen> {
+  // Esta variable almacenará los datos obtenidos de la API
+  var data;
+
+  // Método asíncrono para obtener los datos de la API
+  Future<void> fetchData() async {
+    try {
+      final response = await http.get(Uri.parse('http://10.0.2.2:8000/noticias/noticias_completas/'));
+      if (response.statusCode == 200) {
+        setState(() {
+          // Decodifica el JSON y actualiza el estado
+          final decodedResponse = jsonDecode(utf8.decode(response.bodyBytes));
+          data = decodedResponse;
+        });
+        //print('--------------------');
+        //print(data); // Imprime la respuesta en consola
+        //print('--------------------');
+      } else {
+        print('Error: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData(); // Llama a fetchData() cuando la pantalla se construye
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      appBar: AppBar(
+        title: Text('AdoptMe'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          /*
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {
+              // Acción para refrescar la lista si se desea.
+              controller.fetchPosts();
+            },
+          ),
+          */
+        ],
+      ),
+      
+      body: Center(
+        child: data == null
+            ? CircularProgressIndicator()  // Muestra un indicador de carga mientras los datos se obtienen
+            : ListView.builder(
+                itemCount: data['noticias'].length,
+                itemBuilder: (context, index) {
+                  /*
+                  return ListTile(
+                    contentPadding: EdgeInsets.all(12.0),  // Espaciado dentro del ListTile
+                    title: Text(
+                      data['noticias'][index]['titulo'],
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),  // Estilo de texto del título
+                    ),
+                    subtitle: Text(
+                      data['noticias'][index]['fecha'] + ' ' + data['noticias'][index]['hora'],
+                      style: TextStyle(color: Colors.grey[700]),  // Estilo de texto para la fecha y hora
+                    ),
+                    leading: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),  // Esquinas redondeadas en la imagen
+                      child: Image.network(
+                        data['noticias'][index]['imagen'],
+                        width: 50,  // Tamaño de la imagen
+                        height: 50,
+                        fit: BoxFit.cover,  // Ajustar imagen
+                      ),
+                    ),
+                  );
+                  */
+                  return InkWell(
+                    onTap: () async {
+                      final url = data['noticias'][index]['link']; // Reemplaza con el enlace que deseas
+                      if (await canLaunch(url)) {
+                        await launch(url);
+                      } else {
+                        throw 'No se pudo abrir el enlace: $url';
+                      }
+                    },
+                    child: Card(
+                      child: SizedBox(
+                        height: 100,
+                        child: Row(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Image.network(
+                                data['noticias'][index]['imagen'],
+                                width: 100,
+                                height: 100,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            SizedBox(width: 8.0),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(data['noticias'][index]['titulo'],
+                                      style: TextStyle(fontWeight: FontWeight.bold)),
+                                  Text(data['noticias'][index]['fecha'] + ' ' + data['noticias'][index]['hora']),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+
+                },
+              ),
+      ),
+      
+    );
+  }
+}
+*/
+
+
+/*
+// crea una clase FeedScreen que extiende un StatelessWidget
+class FeedScreen extends StatefulWidget {
+  @override
+  _FeedScreenState createState() => _FeedScreenState();
+}
+
+
+class _FeedScreenState extends State<FeedScreen> {
+  // Esta variable almacenará los datos obtenidos de la API
+  var data;
+
+  // Método asíncrono para obtener los datos de la API
+  Future<void> fetchData() async {
+    try {
+      final response = await http.get(Uri.parse('http://10.0.2.2:8000/noticias/noticias_facebook/'));
+      if (response.statusCode == 200) {
+        setState(() {
+          // Decodifica el JSON y actualiza el estado
+          final decodedResponse = jsonDecode(utf8.decode(response.bodyBytes));
+          data = decodedResponse;
+        });
+        //print('--------------------');
+        //print(data); // Imprime la respuesta en consola
+        //print('--------------------');
+      } else {
+        print('Error: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData(); // Llama a fetchData() cuando la pantalla se construye
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      appBar: AppBar(
+        title: Text('AdoptMe'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          /*
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {
+              // Acción para refrescar la lista si se desea.
+              controller.fetchPosts();
+            },
+          ),
+          */
+        ],
+      ),
+      
+      body: Center(
+        child: data == null
+            ? CircularProgressIndicator()  // Muestra un indicador de carga mientras los datos se obtienen
+            : ListView.builder(
+                itemCount: data['noticias'].length,
+                itemBuilder: (context, index) {
+                  /*
+                  return ListTile(
+                    contentPadding: EdgeInsets.all(12.0),  // Espaciado dentro del ListTile
+                    title: Text(
+                      data['noticias'][index]['titulo'],
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),  // Estilo de texto del título
+                    ),
+                    subtitle: Text(
+                      data['noticias'][index]['fecha'] + ' ' + data['noticias'][index]['hora'],
+                      style: TextStyle(color: Colors.grey[700]),  // Estilo de texto para la fecha y hora
+                    ),
+                    leading: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),  // Esquinas redondeadas en la imagen
+                      child: Image.network(
+                        data['noticias'][index]['imagen'],
+                        width: 50,  // Tamaño de la imagen
+                        height: 50,
+                        fit: BoxFit.cover,  // Ajustar imagen
+                      ),
+                    ),
+                  );
+                  */
+                  return InkWell(
+                    onTap: () async {
+                      final url = data['noticias'][index]['imagenURL']; // Reemplaza con el enlace que deseas
+                      if (await canLaunch(url)) {
+                        await launch(url);
+                      } else {
+                        throw 'No se pudo abrir el enlace: $url';
+                      }
+                    },
+                    
+                    child: Card(
+                      // separar cada card por 15 pixeles
+                      margin: EdgeInsets.all(15),
+                      child: SizedBox(
+                        //height: 100,
+                        child: Row(
+                          children: [
+                            
+                            /*
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Image.network(
+                                data['noticias'][index]['imagenURL'],
+                                width: 100,
+                                height: 100,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            */
+                            
+                            SizedBox(width: 10.0),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                
+                                children: [
+                                  Text(data['noticias'][index]['resu_noticia'],
+                                      style: TextStyle(fontWeight: FontWeight.bold)),
+                                  Text(data['noticias'][index]['date_noticia']),
+                                ],
+                                
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+
+                },
+              ),
+      ),
+      
+    );
+  }
+}
+*/
+
+
+/*
 class FeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -36,6 +506,7 @@ class FeedScreen extends StatelessWidget {
                 itemCount: controller.posts.length,
                 itemBuilder: (context, index) {
                   var post = controller.posts[index];
+                  
                   return Padding(
                     padding: const EdgeInsets.all(12.0),
                     child: Card(
@@ -103,6 +574,7 @@ class FeedScreen extends StatelessWidget {
                       ),
                     ),
                   );
+                  
                 },
               );
               
@@ -113,3 +585,4 @@ class FeedScreen extends StatelessWidget {
     );
   }
 }
+*/
